e7dc7e7f5691a14dff2f3eaa105d9d48b2bb9c48
Misaianes <sairebautista@gmail.com>
2025-11-19 21:13:35 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Servidor funcionando correctamente!");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
